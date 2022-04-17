b6387a3769c7d5bebcab6b974ec8bc16e5423174
sahaj <sahajjain13@gmail.com>
2022-04-18 00:37:07 +0530

    Add LevelHeader to display level's character info

---
+++ src/components/LevelHeader/LevelHeader.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './LevelHeader.css';
+
+function LevelHeader({ characters }) {
+  return (
+    <div className="level-header">
+      <div className="characters-container">
+        {characters.map((character) => (
+          <div key={character.ID} className="character-wrapper">
+            <img src={character.imageURL} alt="" />
+            <div className="character-name">{character.name}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+LevelHeader.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  characters: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default LevelHeader;
