9490c3716b5374b4c94deb55da80ee4ef57d0aba
sahaj <sahajjain13@gmail.com>
2022-04-17 23:13:25 +0530

    Fetch levels data from firestore

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { useEffect, useState } from 'react';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
 import './App.css';
+import initializeFirebaseApp from './firebase';
+import Level from './components/pages/Level/Level';
 
 function App() {
+  const [levels, setLevels] = useState([]);
+
+  useEffect(() => {
+    initializeFirebaseApp();
+    const fetchLevels = async () => {
+      const querySnapshot = await getDocs(collection(getFirestore(), 'levels'));
+      querySnapshot.forEach((doc) => {
+        setLevels(levels.concat({ ...doc.data(), ID: doc.id }));
+      });
+    };
+    fetchLevels();
+  }, []);
+
   return (
-    <div>Hello</div>
+    <div>
+      {levels.length && levels.map((level) => <Level levelID={level.ID} />)}
+    </div>
   );
 }
 
