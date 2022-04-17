c9d2439b31314742b22f7c592a0340bf2350c7c2
sahaj <sahajjain13@gmail.com>
2022-04-17 18:45:10 +0530

    Initialise firebase and firebase-config

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBg0LqW_XvdOK20UDRREE5R6PK8-hkdqdk',
+  authDomain: 'phototaggingapp-81880.firebaseapp.com',
+  projectId: 'phototaggingapp-81880',
+  storageBucket: 'phototaggingapp-81880.appspot.com',
+  messagingSenderId: '427015491326',
+  appId: '1:427015491326:web:efd9c117836ef817545faf',
+};
+
+initializeApp(firebaseConfig);
