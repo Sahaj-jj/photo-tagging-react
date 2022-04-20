83d6b60402f26e49f74a1a76a67ad6c0716c3b3a
sahaj <sahajjain13@gmail.com>
2022-04-20 11:50:22 +0530

    Add level name to level header

--- src/components/LevelHeader/LevelHeader.jsx
+++ src/components/LevelHeader/LevelHeader.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import './LevelHeader.css';
 import Timer from '../Timer/Timer';
 
-function LevelHeader({ characters }) {
+function LevelHeader({ name, characters }) {
   return (
     <div className="level-header">
       <div className="characters-container">
@@ -14,12 +14,14 @@ function LevelHeader({ characters }) {
           </div>
         ))}
       </div>
+      <div className="name">{name}</div>
       <Timer />
     </div>
   );
 }
 
 LevelHeader.propTypes = {
+  name: PropTypes.string.isRequired,
   // eslint-disable-next-line react/forbid-prop-types
   characters: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
