f2120b3b3da638f8353246c52a110963120e359c
sahaj <sahajjain13@gmail.com>
2022-04-21 04:07:11 +0530

    Add useNavigate hook for back button to Home page

--- src/components/LevelHeader/LevelHeader.jsx
+++ src/components/LevelHeader/LevelHeader.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './LevelHeader.css';
+import { useNavigate } from 'react-router-dom';
 import { getFormattedTime } from '../../utils';
 
 function LevelHeader({ name, time }) {
+  const navigate = useNavigate();
+
   return (
     <div className="level-header">
-      <button type="button" className="back">{'<-'}</button>
+      <button type="button" className="back" onClick={() => { navigate('../'); }}>{'<-'}</button>
       <div className="name">{name}</div>
       <div className="timer">
         {getFormattedTime(time)}
