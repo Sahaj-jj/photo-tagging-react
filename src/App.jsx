0f4941ada609de357908b9e7476ce31d27ea2a5f
sahaj <sahajjain13@gmail.com>
2022-04-21 04:04:15 +0530

    Add react router to and fetch character collection from App

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,49 @@
 import React, { useEffect, useState } from 'react';
 import { getFirestore, collection, getDocs } from 'firebase/firestore';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import initializeFirebaseApp from './firebase';
+import Home from './components/pages/Home/Home';
 import Level from './components/pages/Level/Level';
 
 function App() {
   const [levels, setLevels] = useState([]);
+  const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchCollection = async (collectionName) => {
+    const querySnapshot = await getDocs(collection(getFirestore(), collectionName));
+    const data = [];
+    querySnapshot.forEach((doc) => {
+      data.push({ ...doc.data(), ID: doc.id });
+    });
+    return data;
+  };
 
   useEffect(() => {
     initializeFirebaseApp();
     const fetchLevels = async () => {
-      const querySnapshot = await getDocs(collection(getFirestore(), 'levels'));
-      querySnapshot.forEach((doc) => {
-        setLevels(levels.concat({ ...doc.data(), ID: doc.id }));
-      });
+      const levelsData = await fetchCollection('levels');
+      setLevels(levelsData);
     };
+
+    const fetchCharacters = async () => {
+      const charactersData = await fetchCollection('characters');
+      setCharacters(charactersData);
+      setLoading(false);
+    };
+
     fetchLevels();
+    fetchCharacters();
   }, []);
 
   return (
-    <div>
-      {levels.length && levels.map((level) => <Level levelID={level.ID} />)}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={!loading && <Home levels={levels} characters={characters} />} />
+        <Route path="/levels/:levelID" element={<Level />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
